a2391e177de08698456c169e49d9ce193aef8399
lachlanjaimc <90922963+lachlanjaimc@users.noreply.github.com>
2021-10-27 14:30:46 +1030

    initial commit

---
+++ database/db.sql
@@ -0,0 +1,10 @@
+DROP DATABASE db_forum;
+CREATE DATABASE db_forum;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'cont4';
+
+USE db_forum;
+
+CREATE TABLE post (
+    Poster varchar(255),
+    Post text
+);
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mariadb');
+
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'cont4',
+  database: 'db_forum'
+});
+pool.getConnection();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+});
+
+/* GET home page. */
+router.get('/get-comment', async function(req, res, next) {
+  res.set('Content-Type', 'text/html');
+  var rows = await pool.query("select * from post");
+  var string = "";
+  for (let i = 0; i < rows.length; i++) {
+    string += "<h2>";
+    string += rows[i].Poster;
+    string += "</h2>";
+    string += "<p>";
+    string += rows[i].Post;
+    string += "</p>";
+  }
+  res.send(string);
+});
+
+router.post('/post-comment', function(req, res, next) {
+
+  pool.query("insert into post values ('"+ req.body.name + "', '" + req.body.comment + "');", function (err, rows) {
+    if (err) throw err
+  })
+
+  res.redirect('/');
+})
+
+module.exports = router;
